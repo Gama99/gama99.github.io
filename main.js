826bba2e0798c9a706d4f3ff99fea3a9046177f8
Leonardo Apollaro Gama <70278732+Gama99@users.noreply.github.com>
2022-02-24 14:42:39 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const ewelink = require('ewelink-api');
+
+const connection = new ewelink({
+email: 'leo.apollaro@gmail.com',
+password: '_T4w@PUapouw',
+region: 'us',
+});
+
+  /* toggle device */
+async function toggleFan() {
+    await connection.toggleDevice('1000997eff');
+}
+
+/* This fufcntion toggle the device, we need to call this function by using some wtahc feature to trigger*/
+toggleFan();
